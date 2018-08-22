8975dac754f925c976c7c3767880645f5f558396
Kachan Sergey <s.kachan@silencatech.com>
2018-08-22 08:38:40 +0300

    Fix calculation to use pool

--- calcadvise.py
+++ calcadvise.py
@@ -1,9 +1,11 @@
 import argparse
 from _datetime import datetime
+from multiprocessing.pool import Pool
 from time import time
 
 import pytz as pytz
 
+from config import FileConfig
 from models.Advise import Advise
 from services.DbHelper import DbHelper
 from services.Macd import MacD
@@ -12,13 +14,28 @@ from services.Rate import Rate
 def pretty_date(timestamp: int):
     return datetime.fromtimestamp(timestamp, tz=pytz.UTC).strftime('%F %T')
 
-def params_generator():
-    for period in [30, 60, 120, 240, 1440]:
-        for slow in range(10, 41):
-            for fast in range(2, 31):
-                for signal in range(2,21):
-                    if fast + 2 <= slow:
-                        yield fast, slow, signal, period
+def pool_data_gen(period, rates):
+    for slow in range(10, 41):
+        for fast in range(2, 31):
+            for signal in range(2,21):
+                if fast + 2 <= slow:
+                    yield fast, slow, signal, period, dict(rates)
+
+def pool_processor(fast, slow, signal, period, rates):
+    db = DbHelper(fast, slow, signal, period)
+    max_ts = db.get_max_ts()
+    for cts, price in rates.items():
+        cts = int(cts)
+        price = float(price)
+
+        if cts > max_ts:
+            new = Advise(ts=cts, close=price)
+            last = db.fetch_last(cts)
+            if last is not None:
+                new = MacD(fast, slow, signal).calculateNext(cts, price, last)
+            db.save(new)
+    del db
+    del rates
 
 def combine_rate(minute_rates, period_rates):
     result = dict(minute_rates)
@@ -27,56 +44,26 @@ def combine_rate(minute_rates, period_rates):
     return result
 
 if __name__ == '__main__':
-    ts = time()
+    end = 1514764800#time()
 
-    end = time()
+    config = FileConfig()
 
-    rates = Rate().fetch_close(0, end)
+    period = config.get('APP.PERIOD', 30, int)
 
-    period_rates = dict()
-    counter = 0
-    counter_2 = 0
-    for params in params_generator():
-        counter += 1
-        fast, slow, signal, period = params
-        db = DbHelper(*params)
+    minute_prices = Rate().fetch_close(0, end)
+    prices = Rate().fetch_close(0, end, period)
 
-        p_rates = None
-        if period in period_rates:
-            p_rates = period_rates[period]
-        else:
-            p_rates = Rate().fetch_close(0, end, period)
-            period_rates[period] = period_rates
+    combined_prices = combine_rate(minute_prices, prices)
 
-        max_ts = db.get_max_ts()
+    process_count = config.get('APP.POOL_PROCESSES', 4, int)
+    max_tasks = config.get('APP.POOL_TASK_PER_CHILD', 10, int)
+    start_at = time()
+    pool = Pool(processes=process_count, maxtasksperchild=max_tasks)
+    pool.starmap(pool_processor, pool_data_gen(period, combined_prices))
+    pool.close()
+    pool.join()
+    print(f'Done in {time() - start_at} s. Total: {len(list(combined_prices))}')
+    #print(f'Done')
 
-        res_rates = combine_rate(rates, p_rates)
-        counter_2 = 0
-        ts2 = time()
-        for ts, close in res_rates.items():
-            counter_2 += 1
-            ts = int(ts)
-            close = float(close)
-
-            if ts > max_ts:
-                new = Advise(ts=ts, close=close)
-                last = db.fetch_last(ts)
-                if last is not None:
-                    new = MacD(fast, slow, signal).calculateNext(ts, close, last)
-                db.save(new)
-            else:
-                print(f'Skipped for {fast}, {slow}, {signal}, {period} ...')
-
-            if counter_2%1000 == 0:
-                print(f'{counter_2} rates processed')
-        if counter%10 == 0:
-            print(f'{counter} combinations processed')
-        print(f'Spent {time() - ts2} seconds')
-        del db
-        del res_rates
-    exit('Done');
-
-    print(f'There are {len(list(rates))}. Spent {time() - ts} seconds');
-    exit()
 
 
